58e7d8647691c55512227955663e5d39d0ba2d3f
Archeron13 <dzhaira.a@phystech.edu>
2022-11-22 22:02:49 +0300

    Added Functionality for choosing the text

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from pynput import keyboard
 import sys
 import tkinter
+from tkinter import filedialog
 from tkinter.messagebox import YES
 import customtkinter as ctk
 from PIL import Image, ImageTk
@@ -10,22 +10,16 @@ from threading import Thread
 
 WIDTH = 1280
 HEIGHT = 720
-
-with open("test.txt") as f:             
-    sentence = f.read()
-    #char_sentence = list(sentence)
-    list_sentences = re.split(r'[ \n]',sentence)
-    char_sentence = sentence.replace("\n"," ")
-    print(char_sentence)
+DEFAULT_TEXT = ("default files/default_input.txt")
 
 
 
-
+#Class for resizing background image
 class background(tkinter.Canvas):
     def __init__(self, master, *pargs):
         tkinter.Canvas.__init__(self, master, *pargs)
 
-        self.imgBG = Image.open("Images/startBG.jpg")
+        self.imgBG = Image.open("default files/startBG.jpg")
         self.imgBG_copy = self.imgBG.copy()
 
 
@@ -36,7 +30,6 @@ class background(tkinter.Canvas):
         self.background.bind('<Configure>', self._resize_image)
 
     def _resize_image(self,event):
-
 
         self.imgBG = self.imgBG_copy.resize((event.width, event.height))
 
@@ -60,42 +53,89 @@ class startGUI:
         self.bgCanvas = background(self.root)
         self.bgCanvas.place(x=0,y=0,relwidth=1,relheight=1)
 
-        self.text_var = list_sentences[:20]
-        self.input_var = ""
+        self.last_index_of_list = 50 # End index of shown list
 
+        with open(DEFAULT_TEXT) as f:             
+            sentence = f.read()
+            self.list_sentences = re.split(r'[ \n]',sentence)
+            self.char_sentence = sentence.replace("\n"," ")
+
+        self.text_var = self.char_sentence[:self.last_index_of_list]
+        self.input_var = ""
         self.cur_key = ""
+        self.game_running = False
 
        # self.mainTextLabel = ctk.CTkLabel(self.root,text = "Keyboard Simulator",text_font = ("Arial50,20"),fg_color=("white","grey"))
         # self.mainTextLabel.pack(pady="10",fill="x")
 
-        self.showTextLabel = ctk.CTkLabel(self.root,text =self.text_var,text_font=("Arial50",12))
-        self.showTextLabel.pack(fill="x",anchor="center")
+        #self.text_label = ctk.CTkLabel(self.root, text= "")
+
+        self.showTextLabel = ctk.CTkLabel(self.root,text =self.text_var,text_font=("Arial50",15))
+        self.showTextLabel.pack( padx=20, pady= 20,fill="both")
+
+        self.input_label = ctk.CTkLabel(self.root,text="Press Start",text_color="Red",text_font=("Arial50,12"))
+        self.input_label.pack(padx=20, pady= 20)
+
+        self.inputTextLabel = ctk.CTkLabel(self.root,text =self.input_var,text_font=("Arial50","15"),text_color="black",fg_color=("white"))
+        self.inputTextLabel.pack(padx=20, pady= 20,fill="both")
+
+        self.result_label = ctk.CTkLabel(self.root,text=(f'Keystroke: 0\n'
+                                                         f'Wrong Keystroke: 0\n'
+                                                         f'Character Per Seconds : 0\n'
+                                                         f'Character Per Minute: 0\n'
+                                                         f'Accuracy: 0\n' )
+                                                         ,text_font=("Arial50,15"))
+        self.result_label.pack(side="right", padx=20, pady= 20,)
+
+        self.button_frame = ctk.CTkFrame(self.root)
+        self.button_frame.pack(side="left",padx=20,pady=20)
+
+        self.start_button = ctk.CTkButton(self.button_frame, text="Start!", command=Thread(target=self.start_game).start,
+                                         fg_color=("grey"),text_color="black",text_font=("Arial50,15"))
+        self.start_button.pack(side = "top", padx=20, pady= 20)
+        self.browse_button = ctk.CTkButton(self.button_frame,text = "Browse",fg_color=("grey"),text_color="black"
+                                          ,text_font=("Arial50,15"),command = self.browse)
+        self.browse_button.pack(side="top",padx= 20, pady=20)
+
+        self.restart_button = ctk.CTkButton(self.button_frame, text="Restart",fg_color=("grey"),text_color="black"
+                                            ,text_font=("Arial50,15"))
+        self.restart_button.pack(side = "bottom",padx=20, pady= 20)
 
 
-        self.inputTextLabel = ctk.CTkLabel(self.root,text =self.input_var,text_font=("Arial50","12"),text_color="white",fg_color=("black","grey"))
-        self.inputTextLabel.pack(fill="x",expand = True)
-
-        self.button = ctk.CTkButton(self.root, text="Press to Start", command=Thread(target=self.start_game).start)
-
-        self.button.pack(padx=20, pady=40)
 
         #STatistics for game
-        self.wrong_counter = 0
-        self.key_count = 0
-        self.char_count = 0
+        self.total_counter = 0
+        self.correct_count = 0
         self.cur_state = ""
         self.cur_keycode = 0
+        self.time_taken = 0
         self.root.mainloop()
+    
+    def browse(self):
+        file = tkinter.filedialog.askopenfile(mode='r', filetypes=[('Text Files', '*.txt')])
+        if file:
+            sentence = file.read()
+            self.list_sentences = re.split(r'[ \n]',sentence)
+            self.char_sentence = sentence.replace("\n"," ")
+            self.showTextLabel.config(text=self.char_sentence[:self.last_index_of_list])
+
+
+
     
     def key_press(self,event):
         self.cur_key = event.char
         self.cur_state= event.state
         self.cur_keycode = event.keycode
-        self.key_count += 1
+        self.total_counter += 1
 
     def start_game(self):
         self.root.bind("<KeyPress>",self.key_press)
-        for cur_word in char_sentence:
+        start_time = time.time()
+        word_index = 0 #Used to see which word to remove that has already been iterated
+        print(self.char_sentence)
+        for cur_word in self.char_sentence:
+            self.last_index_of_list += 1
+            word_index += 1
             if (len(self.input_var) >= 60):
                 for x in range(35,50):
                     print(x)
@@ -103,23 +143,34 @@ class startGUI:
                         self.input_var= self.input_var[x:]
                         self.inputTextLabel.configure(text = self.input_var)
                         break
-            for curChar in cur_word:
-                exit_loop = False
-                if (exit_loop):
+            if (cur_word== " "):
+                self.input_label.configure(text = f'Space')
+                if (self.last_index_of_list > len(self.char_sentence)):
+                    self.last_index_of_list = len(self.char_sentence)
+                self.showTextLabel.configure(text = self.char_sentence[word_index:self.last_index_of_list])
+            else:
+                self.input_label.configure(text = f'{cur_word} ')
+            exit_loop = False
+            if (exit_loop):
+                break
+            print(f"{cur_word} IS THE BUTTON TO PRESS DUMB FUCK")
+            while True: 
+                if (cur_word == self.cur_key):
+                    self.correct_count += 1
+                    self.input_var+=(cur_word)
+                    self.inputTextLabel.configure(text =self.input_var)
                     break
-                print(f"{curChar} IS THE BUTTON TO PRESS DUMB FUCK")
-                while True:
-                    if (curChar == self.cur_key):
-                        self.char_count += 1
-                        self.input_var+=(curChar)
-                        self.inputTextLabel.configure(text =self.input_var)
-                        break
-                    elif (self.cur_state == 17 and self.cur_keycode == 52 ):
-                        exit_loop = False
-                        break
-                    else:
-                        pass
-        print(f"{self.char_count} cur_count, {self.key_count} key_count, {self.key_count - self.char_count} wrong count")
+                elif (self.cur_state == 4 and self.cur_keycode == 52 ):
+                    exit_loop = False
+                    break
+                else:
+                    pass
+        self.time_taken = time.time() - start_time 
+        self.result_label.configure(text=(f'Keystroke: {self.correct_count}\n'
+                                          f'Wrong Keystroke: {self.total_counter - self.correct_count}\n'
+                                          f'Character Per Seconds  {self.correct_count/self.time_taken:.2f}\n'
+                                          f'Character Per Minute: {self.correct_count/self.time_taken * 60:.2f}\n'
+                                          f'Accuracy: {self.correct_count/self.total_counter * 100:.2f}'))
 
 
 startGUI()
